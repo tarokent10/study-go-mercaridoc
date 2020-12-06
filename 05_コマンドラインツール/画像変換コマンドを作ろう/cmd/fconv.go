366d58ec25ebaef98fd2ad3032fd5990d224341a
kentaro.watanabe <t.8.p.1.dogg@gmail.com>
2020-12-06 17:25:26 +0900

    画像変換コマンド

---
+++ 05_コマンドラインツール/画像変換コマンドを作ろう/cmd/fconv.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/tarokent10/go-utils/imgutil"
+)
+
+// fconv converts file type.
+// useage
+// fconv [directory name] {-s [source type(default jpeg)] -d [dest type(default png)]}
+func main() {
+	args := new(argsMgr)
+	args.parse()
+	fmt.Printf("convert files in %s(%s -> %s)", args.dirname, args.srcType, args.destType)
+	if err := convert(args.srcType, args.destType, args.dirname); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func convert(s, d filetype, dir string) error {
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if dir != path && info.IsDir() {
+				// 自身もwalk対象となるのでチェックしないと無限ループになる
+				return convert(s, d, path)
+			}
+			pos := strings.LastIndex(path, ".")
+			if path[pos:] == s.withExt() {
+				return convertFile(s, d, path)
+			}
+			return nil
+		})
+	return err
+}
+func convertFile(s, d filetype, path string) error {
+	var err error
+	if s == fileTypePNG {
+		switch d {
+		case fileTypeJPEG:
+			_, err = imgutil.Png2Jpeg(path)
+		case fileTypeGIF:
+			_, err = imgutil.Png2Gif(path)
+		}
+	} else if s == fileTypeJPEG {
+		switch d {
+		case fileTypePNG:
+			_, err = imgutil.Jpeg2Png(path)
+		case fileTypeGIF:
+			_, err = imgutil.Jpeg2Gif(path)
+		}
+	} else if s == fileTypeGIF {
+		switch d {
+		case fileTypePNG:
+			_, err = imgutil.Gif2Png(path)
+		case fileTypeJPEG:
+			_, err = imgutil.Gif2Jpeg(path)
+		}
+	}
+	return err
+}
+
+type filetype string
+
+func (f *filetype) is(s string) bool {
+	return string(*f) == strings.ToLower(s)
+}
+
+func (f *filetype) withExt() string {
+	return fmt.Sprintf(".%s", string(*f))
+}
+
+const (
+	fileTypePNG  filetype = "png"
+	fileTypeJPEG filetype = "jpeg"
+	fileTypeGIF  filetype = "gig"
+)
+
+type argsMgr struct {
+	dirname  string
+	srcType  filetype
+	destType filetype
+}
+
+func (m *argsMgr) parse() {
+	// default
+	src := fileTypeJPEG
+	dest := fileTypePNG
+
+	s := flag.String("s", "jpeg", "fconv -s jpeg")
+	d := flag.String("d", "png", "fconv -d png")
+	flag.Parse()
+	args := flag.Args()
+
+	if !m.validate(s, d, args) {
+		log.Fatal("arguments are not valid!!")
+	}
+	if s != nil {
+		src, _ = m.toFileType(*s)
+	}
+	if d != nil {
+		dest, _ = m.toFileType(*d)
+	}
+
+	m.dirname = args[0]
+	m.srcType = src
+	m.destType = dest
+
+}
+func (m *argsMgr) validate(s, d *string, args []string) bool {
+	// 冗長なのでもう少しスマートに書きたかった..
+	if len(args) != 1 {
+		println("no or too many args!")
+		return false
+	}
+
+	if s != nil {
+		if _, err := m.toFileType(*s); err != nil {
+			println(err.Error())
+			return false
+		}
+	}
+	if d != nil {
+		if _, err := m.toFileType(*d); err != nil {
+			println(err.Error())
+			return false
+		}
+	}
+
+	return true
+}
+
+func (m *argsMgr) toFileType(s string) (filetype, error) {
+	for _, ft := range []filetype{fileTypePNG, fileTypeJPEG, fileTypeGIF} {
+		if ft.is(s) {
+			return ft, nil
+		}
+	}
+	return "", errors.New("no such file type")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module study-go--mercaridoc
 go 1.15
 
 require (
-	github.com/tenntenn/greeting v1.0.0 // indirect
-	github.com/tenntenn/greeting/v2 v2.1.0 // indirect
+	github.com/tarokent10/go-utils v0.0.1
+	github.com/tenntenn/greeting v1.0.0
+	github.com/tenntenn/greeting/v2 v2.1.0
+	golang.org/x/tools v0.0.0-20201204222352-654352759326 // indirect
 )
